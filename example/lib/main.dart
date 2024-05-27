7620ee4578212eaadf24ca920846e940006d156a
emrhnzngn <emrhnzngn@hotmail.com>
2024-05-27 17:35:26 +0300

    example comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,9 +6,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,18 +24,23 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// Declares a Video variable.
 late Video video;
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
-    video = Video(videoId: "mKdjycj-7eE", captionLanguageCode: "en", setLoop: false);
+    // Initializes the video variable.
+    video = Video(
+        videoId: "mKdjycj-7eE", captionLanguageCode: "en", setLoop: false);
     setState(() {});
     super.initState();
   }
@@ -44,10 +50,15 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Center(
         child: YoutubeCaptionedPlayer(
+          // Sets the video for YoutubeCaptionedPlayer.
           video: video,
+          // Specifies whether the UI elements should be displayed.
           isUi: true,
+          // Specifies whether the captions should be displayed.
           caption: true,
+          // Specifies whether the sound should be on or off.
           sound: true,
+          // Specifies whether scrubbing through the video should be allowed.
           allowScrubbing: true,
         ),
       ),
